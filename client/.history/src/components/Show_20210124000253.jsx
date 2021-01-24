2cacf4cc539469b0b012f59b155768802aabbc70
vinod kumar <vinod513kumar@gmail.com>
2021-01-24 00:59:16 -0500

    CSS:80%completed

---
+++ client/.history/src/components/Show_20210124000253.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useParams } from "react-router-dom";
+
+function Show(props) {
+  const params = useParams()
+
+  const wine = props.images.find((wines) => {
+    return wines.id === params.id
+    
+  })
+  console.log(wine)
+
+  if (!wine) {
+    return <p> Loading ...</p>;
+  } else {
+    return (
+      <div className="showImage">
+        <img src={wine.fields.image} alt={wine.name} height="180px" width="0px"/>
+        <div className="showInfo">
+        <h2>{wine.fields.name}</h2>
+        <h2>{wine.fields.varietal}</h2>
+        <h3>{wine.fields.price}</h3>
+      
+        <a href={wine.fields.description} target="_blank">
+          <button>
+            Read More
+          </button>
+        </a>
+        </div>
+      </div>
+    )
+  }
+}
+export default Show;
---
+++ client/.history/src/components/Show_20210124004231.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useParams } from "react-router-dom";
+
+function Show(props) {
+  const params = useParams()
+
+  const wine = props.images.find((wines) => {
+    return wines.id === params.id
+    
+  })
+  console.log(wine)
+
+  if (!wine) {
+    return <p> Loading ...</p>;
+  } else {
+    return (
+      <div className="showImage">
+        <img src={wine.fields.image} alt={wine.name} height="350px" width="280px"/>
+        <div className="showInfo">
+        <h2>Name: {wine.fields.name}</h2>
+        <h2>Varietal: {wine.fields.varietal}</h2>
+        <h3>Price: ${wine.fields.price}</h3>
+      
+        <a href={wine.fields.description} target="_blank">
+          <button clas>
+            Read More
+          </button>
+        </a>
+        </div>
+      </div>
+    )
+  }
+}
+export default Show;
