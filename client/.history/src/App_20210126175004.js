174362cc6b2596113d37ce0f8417aaf595311c14
vinod kumar <vinod513kumar@gmail.com>
2021-01-26 18:26:35 -0500

    code clean-up completed

---
+++ client/.history/src/App_20210126175004.js
@@ -0,0 +1,58 @@
+import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { baseURL, config } from "./services";
+import { Link, Route } from "react-router-dom";
+import Show from "./components/Show";
+import Home from "./components/Home";
+import Form from "./components/Form";
+import Nav from "./components/Nav";
+import Review from "./components/Review";
+import Footer from "./components/Footer";
+
+function App() {
+  const [images, setImages] = useState([]);
+  const [toggle, setToggle] = useState(false)
+
+  useEffect(() => {
+    const getData = async () => {
+      const resp = await axios.get(baseURL, config);
+      setImages(resp.data.records)
+    };
+    getData();
+  }, [toggle]);
+
+  return (
+      
+    <html className="backGroundImage">
+      <div className="App">
+        <header className="title">Spot The Wine  </header>
+        
+        <Nav />
+          <div className="homeImg">
+          
+          <Route exact path="/">
+                <Home images={images} />
+               </Route>
+            </div>
+
+        <Route path="/images/:id">
+            <Show images={images} setToggle={setToggle} />
+         </Route>
+
+        <Route path="/new">
+            <Form images={images} toggle={toggle} setToggle={setToggle} />
+          </Route>
+          
+          <Route path="/edit/:id">
+              <Review key={images.id} images={images} setToggle={setToggle} />
+          </Route>
+        </div>
+        <Footer />
+  
+      </html>
+    
+  );
+}
+
+export default App;
---
+++ client/.history/src/components/Home_20210126182249.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+
+function Home(props) {
+
+  return (
+  
+    <div className="wineCard">
+        {
+          props.images.map((imag) => (
+            <Link  to={`/images/${imag.id}`}>
+              <img className="wineImg" src={imag.fields.image} alt={imag.name} />
+            </Link>))
+         }</div>
+      // </div>
+  )
+}
+
+export default Home;
+
